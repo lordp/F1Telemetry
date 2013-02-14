1bea6664b3849d9acbdba6099cf88b46ca2fbbf3
Finn Grimwood <fgrimwood@solarflare.com>
2013-02-14 13:16:28 +0000

    Created socket handler thread

--- socket_handler.py
+++ socket_handler.py
@@ -1,25 +1,43 @@
+#!/usr/bin/env python
+
 import struct
 from socket import *
 from structures import *
+import threading
+
+class SocketThread(threading.Thread):
+    def __init__(self, session):
+        threading.Thread.__init__(self)
+        self.session = session
+        self.running = True
+
+        #open socket
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.socket.bind(('localhost', 20777))
+
+        self.start()
+
+    def close(self):
+        self.running = False
+
+    def run(self):
+        while self.running:
+            #populate a packet object
+            packet = Packet()
+            raw_packet = self.socket.recv(struct.calcsize(packet.format))
+            packet.decode_raw_packet(raw_packet)
+
+            #add this packet object to the current session
+            lap_finished = not self.session.current_lap.add_packet(packet)
+            if lap_finished:
+                pass #signal an event to the GUI
+
+        #we've signalled for the recv thread to stop, so do cleanup
+        self.socket.close()
 
 if __name__ == '__main__':
-    session = Session()
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    s.bind(('localhost', 20777))
-    
-    running = True
-    print "Running" 
-    while running:
-        p = Packet()
-        raw_packet = s.recv(struct.calcsize(p.format))
-        p.decode_raw_packet(raw_packet)
-#       print p.time, p.laptime
-        b = session.current_lap.add_packet(p)
-        if session.fastest_lap:
-            closest_packet = session.fastest_lap.get_closest_packet(p)
-            lap_delta = p.lap_time - closest_packet.lap_time
-            print lap_delta
-        if not b:
-            print "Lap finished", session.current_lap.lap_time
-            session.new_lap()
+    s = Session()
+    thread = SocketThread(s);
+    while True:
+        pass #todo print out some stuff
