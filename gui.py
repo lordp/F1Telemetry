40f3f8f74fcbbbbeabe151858f0064f65c897129
Finn Grimwood <fgrimwood@solarflare.com>
2013-02-14 13:21:09 +0000

    Playing with GUI options

--- gui.py
+++ gui.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 
 import wx
+class ExampleFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent)
+        panel = wx.Panel(self)
+        font = wx.Font(30, wx.FONTFAMILY_TELETYPE, wx.FONTSTYLE_NORMAL, 
+                       wx.FONTWEIGHT_NORMAL)
+        self.quote = wx.StaticText(panel, label="Lap delta: ", 
+                                   pos=(20, 30), size=(20,20))
+        self.quote.SetFont(font)
+        self.Show()
 
 app = wx.App(False)
-frame = wx.Frame(None, wx.ID_ANY, "Hello World!")
-frame.Show(True)
+ExampleFrame(None)
 app.MainLoop()
