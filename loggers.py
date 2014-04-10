890f731d21d86c0e597478a2a3427eb4fb30ff51
Darryl Hamilton <darrylh@catalyst.net.nz>
2014-04-10 13:26:13 +1200

    Extend what is shown on the status bar during data acquisition

--- loggers.py
+++ loggers.py
@@ -23,12 +23,20 @@ class RacingLeagueCharts(Logger):
         self.session_url = 'https://racingleaguecharts.com/sessions/register'
         self.lap_url = 'https://racingleaguecharts.com/laps'
 
+    def update_status(self, lap_time = False):
+        if lap_time:
+            msg = 'Session id: {0}, Last lap: {1}'.format(self.session_id, self.format_time(lap_time))
+        else:
+            msg = 'Session id: {0}'.format(self.session_id)
+
+        self.status_bar.SetStatusText(msg)
+
     def request_session(self, packet):
         payload = { "driver": self.screen_name, "track": packet.track_length, "type": packet.session_type }
         r = requests.post(self.session_url, data=payload, verify=False)
         if r.status_code == 200:
             self.session_id = r.json()['session_id']
-            self.status_bar.SetStatusText('Session id: {0}'.format(self.session_id))
+            self.update_status()
             return True
         return False
 
@@ -39,5 +47,13 @@ class RacingLeagueCharts(Logger):
         payload = { "session_id": self.session_id, "lap_number": lap.lap_number, "sector_1": lap.sector_1, "sector_2": lap.sector_2, "total": lap.lap_time }
         r = requests.post(self.lap_url, data=payload, verify=False)
         if r.status_code == 200:
+            self.update_status(lap.lap_time)
             return True
         return False
+
+    def format_time(self, seconds):
+        m, s = divmod(seconds, 60)
+        if m > 0:
+            return '{0:.0f}:{1:06.3f}'.format(m, s)
+        else:
+            return '{0:06.3f}'.format(s)
