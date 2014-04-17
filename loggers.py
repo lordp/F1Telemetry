7408a5dd4005d1fa437919927e10c683a9daf876
Darryl Hamilton <darryl@addict.net.nz>
2014-04-18 11:07:34 +1200

    Drop parent Logger class, and adjust UI element references

--- loggers.py
+++ loggers.py
@@ -1,51 +1,28 @@
 import requests
 import decimal
 
-class Logger(object):
-    def __init__(self, driver):
-        self.screen_name = driver
-
-    def lap(self, lap):
-        pass
-
-    def delta(self, delta):
-        pass
-
-    def position(self, x, y):
-        pass
-
-class RacingLeagueCharts(Logger):
-    def __init__(self, driver, status_bar, local_mode):
-        super(RacingLeagueCharts, self).__init__(driver)
+class RacingLeagueCharts:
+    def __init__(self, parent):
         self.session_id = 0
-        self.status_bar = status_bar
+        self.parent = parent
         self.log = []
-        self.local_mode = local_mode
 
         self.session_url = 'https://racingleaguecharts.com/sessions/register'
         self.lap_url = 'https://racingleaguecharts.com/laps'
-
-    def update_status(self, lap_time = False):
-        if lap_time:
-            msg = 'Session id: {0}, Last lap: {1}'.format(self.session_id, self.format_time(lap_time))
-        else:
-            msg = 'Session id: {0}'.format(self.session_id)
-
-        self.status_bar.SetStatusText(msg)
 
     def add_log_entry(self, msg):
         self.log.append(msg)
 
     def request_session(self, packet):
-        if self.local_mode:
+        if self.parent.config['local_enabled']:
             return True
         self.add_log_entry('New session requested')
         track_length = decimal.Decimal(packet.track_length)
-        payload = { "driver": self.screen_name, "track": round(track_length, 3), "type": packet.session_type }
+        payload = { "driver": self.parent.config['name'], "track": round(track_length, 3), "type": packet.session_type }
         r = requests.post(self.session_url, data = payload, verify = False)
         if r.status_code == 200:
             self.session_id = r.json()['session_id']
-            self.update_status()
+            self.parent.session_id.SetLabel('Session: {0}'.format(self.session_id))
             self.add_log_entry("Session id: {0}".format(self.session_id))
             return True
         return False
@@ -56,7 +33,8 @@ class RacingLeagueCharts(Logger):
     def lap(self, lap):
         raw_times, formatted_times = self.format_lap_times(lap)
         self.add_log_entry("Lap: {0:02d} {1} {2} {3} {4}".format(int(lap.lap_number), formatted_times['total'], formatted_times['s1'], formatted_times['s2'], formatted_times['s3']))
-        if self.local_mode:
+        self.parent.last_lap.SetLabel('Last Lap: {0}'.format(formatted_times['total']))
+        if self.parent.config['local_enabled']:
             return True
         payload = {
             "session_id": self.session_id, "lap_number": lap.lap_number,
@@ -64,7 +42,6 @@ class RacingLeagueCharts(Logger):
             "speed": lap.top_speed, "fuel": lap.current_fuel, "position": lap.position }
         r = requests.post(self.lap_url, data = payload, verify = False)
         if r.status_code == 200:
-            self.update_status(lap.lap_time)
             return True
         return False
 
